3b2caea0623e0f7000f8130226cc5092b08cbbe5
Anderson Chica Osorio <Anderson Chica Osorio>
2017-05-08 16:58:32 -0500

    add retries page form controls

--- src/app/retry/admin-retry/admin-retry.component.ts
+++ src/app/retry/admin-retry/admin-retry.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-admin-retry',
-  templateUrl: './admin-retry.component.html',
-  styleUrls: ['./admin-retry.component.scss']
+	selector: 'app-admin-retry',
+	templateUrl: './admin-retry.component.html',
+	styleUrls: ['./admin-retry.component.scss']
 })
 export class AdminRetryComponent implements OnInit {
 
-  constructor() { }
+	services: string[] = [];
 
-  ngOnInit() {
-  }
+	form: FormGroup;
 
+	constructor(private route: ActivatedRoute, public fb: FormBuilder,
+		public router: Router) {
+		this.form = this.fb.group({
+			consumer: [''],
+			messageId: [''],
+			initialDate: [''],
+			finalDate: [''],
+			application: [''],
+			service: [''],
+		});
+	}
+
+	ngOnInit() {
+	}
+
+	search() {
+		this.services.push("hola");
+	}
 }
