38d8f6b3eb361eee195f235151ac05f11d797fad
Anderson Chica Osorio <Anderson Chica Osorio>
2017-06-22 14:54:47 -0500

    add adal packages to app

---
+++ src/app/services/config.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class ConfigService {
+
+	constructor() {
+
+	}
+
+	public get getAdalConfig(): any {
+
+		return {
+
+			tenant: 'ENTER YOUR TENANT ID',
+
+			clientId: 'ENTER YOUR CLIENT ID',
+
+			redirectUri: window.location.origin + '/',
+
+			postLogoutRedirectUri: window.location.origin + '/'
+
+		};
+
+	}
+
+} 
